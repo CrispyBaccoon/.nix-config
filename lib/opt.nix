b38310249bb51023d96786506c41cf8da9cd8843
comfysage <67917529+comfysage@users.noreply.github.com>
2024-04-29 19:18:00 +0200

    doc(lib): add inline comments for opt lib fn

--- lib/opt.nix
+++ lib/opt.nix
@@ -1,12 +1,16 @@
 {lib, ...}:
 with lib; rec {
+  # mkOpt type default desc
   mkOpt = type: default: description:
     mkOption {inherit type default description;};
 
+  # mkOpt' type default
   mkOpt' = type: default: mkOpt type default null;
 
+  # mkBoolOpt default desc
   mkBoolOpt = mkOpt types.bool;
 
+  # mkBoolOpt' default
   mkBoolOpt' = mkOpt' types.bool;
 
   mkEnableOpt = mkBoolOpt' false;
