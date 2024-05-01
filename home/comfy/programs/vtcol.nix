daa1fb2139ecdd721b1d76af403fdb416dda6124
comfysage <67917529+comfysage@users.noreply.github.com>
2024-05-01 13:55:09 +0200

    fix(vtcol): use `config.palette`

--- home/comfy/programs/vtcol.nix
+++ home/comfy/programs/vtcol.nix
@@ -1,38 +1,33 @@
-let
-  home = import <home-manager-config>;
-in
-  {
-    pkgs,
-    inputs,
-    lib,
-    config,
-    ...
-  }: {
-    home.file.".vtcol/${config.colorScheme.name}" = let
-      colors = config.colorScheme.colors;
-    in {
-      enable = true;
-      text = ''
-        0#${colors.base00}   base03
-        8#${colors.base08}   base03
-        1#${colors.base01}   base03
-        9#${colors.base09}   base03
-        2#${colors.base02}   base03
-        10#${colors.base0A}  base03
-        3#${colors.base03}   base03
-        11#${colors.base0B}  base03
-        4#${colors.base04}   base03
-        12#${colors.base0C}  base03
-        5#${colors.base05}   base03
-        13#${colors.base0D}  base03
-        6#${colors.base06}   base03
-        14#${colors.base0E}  base03
-        7#${colors.base07}   base03
-        15#${colors.base0F}  base03
-      '';
-    };
-    home.file.".vtcol/theme" = {
-      enable = true;
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.vtcol/${config.colorScheme.name}";
-    };
-  }
+{
+  pkgs,
+  inputs,
+  lib,
+  config,
+  ...
+}: {
+  home.file.".vtcol/${config.colorScheme.name}" = let
+    palette = config.palette;
+  in lib.custom.use {
+    text = ''
+      0#${palette.color0}   base03
+      8#${palette.color8}   base03
+      1#${palette.color1}   base03
+      9#${palette.color9}   base03
+      2#${palette.color2}   base03
+      10#${palette.color10}  base03
+      3#${palette.color3}   base03
+      11#${palette.color11}  base03
+      4#${palette.color4}   base03
+      12#${palette.color12}  base03
+      5#${palette.color5}   base03
+      13#${palette.color13}  base03
+      6#${palette.color6}   base03
+      14#${palette.color14}  base03
+      7#${palette.color7}   base03
+      15#${palette.color15}  base03
+    '';
+  };
+  home.file.".vtcol/theme" = lib.custom.use {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.vtcol/${config.colorScheme.name}";
+  };
+}
