c00af32ce8f3c402af30b192386f38a8a653c4fc
isabel <isabel@isabelroses.com>
2024-05-30 15:36:09 +0100

    refactor(sddm-sugar-dark): refactor sddm package and module

--- hosts/cottage/sddm.nix
+++ hosts/cottage/sddm.nix
@@ -1,11 +1,12 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  self,
+  ...
+}: {
   services.xserver.enable = true;
   services.xserver.displayManager = {
     sddm.enable = true;
-    sddm.theme = "${import ../../pkgs/sddm-sugar-dark/default.nix {inherit pkgs;}}";
+    sddm.theme = "sddm-sugar-dark";
   };
-  environment.systemPackages = with pkgs; [
-    libsForQt5.qt5.qtquickcontrols2
-    libsForQt5.qt5.qtgraphicaleffects
-  ];
+  environment.systemPackages = [self.packages.${pkgs.system}.sddm-sugar-dark];
 }
--- pkgs/sddm-sugar-dark/default.nix
+++ pkgs/sddm-sugar-dark/default.nix
@@ -1,24 +1,47 @@
-{pkgs}: let
-  imgLink = "https://github.com/crispybaccoon/wallpapers/raw/mega/wall/scape.jpg";
+{
+  stdenvNoCC,
+  fetchFromGitHub,
+  fetchurl,
+  libsForQt5,
+  image ?
+    fetchurl {
+      url = "https://github.com/crispybaccoon/wallpapers/raw/mega/wall/scape.jpg";
+      sha256 = "0a5y8h7pjmczf5m7krs3jl4fjmiz7aixf44wmrc4k683239is13h";
+    },
+}:
+stdenvNoCC.mkDerivation {
+  name = "sddm-sugar-dark";
+  version = "change-me-later";
 
-  image = pkgs.fetchurl {
-    url = imgLink;
-    sha256 = "0a5y8h7pjmczf5m7krs3jl4fjmiz7aixf44wmrc4k683239is13h";
+  dontWrapQtApps = true;
+
+  src = fetchFromGitHub {
+    owner = "MarianArlt";
+    repo = "sddm-sugar-dark";
+    rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
+    sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
   };
-in
-  pkgs.stdenv.mkDerivation {
-    name = "sddm-sugar-dark";
-    src = pkgs.fetchFromGitHub {
-      owner = "MarianArlt";
-      repo = "sddm-sugar-dark";
-      rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-      sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
-    };
-    installPhase = ''
-      mkdir -p $out
-      cp -R ./* $out/
-      cd $out/
-      rm Background.jpg
-      cp -r ${image} $out/Background.jpg
-    '';
-  }
+
+  propagatedBuildInputs = with libsForQt5.qt5; [
+    qtquickcontrols2
+    qtgraphicaleffects
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/share/sddm/themes/"
+    cp -R ./* "$out/share/sddm/themes/"
+    cd "$out/share/sddm/themes/"
+    rm Background.jpg
+    cp -r ${image} ./Background.jpg
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    mkdir -p $out/nix-support
+    echo ${libsForQt5.qt5.qtquickcontrols2} >> $out/nix-support/propagated-user-env-packages
+    echo ${libsForQt5.qt5.qtgraphicaleffects} >> $out/nix-support/propagated-user-env-packages
+  '';
+}
