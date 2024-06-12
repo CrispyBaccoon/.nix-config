749dbabb9f1535ae3356faa9e149ca56bcc0f96d
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-12 23:44:41 +0200

    fixup! fix!: don't use system to rebuild home

--- home/default.nix
+++ home/default.nix
@@ -2,4 +2,16 @@
   self,
   ...
 }: {
+  nixpkgs = {
+    overlays = [
+      self.outputs.overlays.additions
+      self.outputs.overlays.modifications
+      self.outputs.overlays.unstable-packages
+    ];
+    config = {
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
 }
