d1f4529fe9bbf3134cb75a716ea137d16e0f21da
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-12 23:42:10 +0200

    fixup! fix!: don't use system to rebuild home

--- home/default.nix
+++ home/default.nix
@@ -1,24 +1,5 @@
 {
   self,
-  self',
-  inputs,
-  inputs',
   ...
 }: {
-  home-manager = {
-    verbose = true;
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    backupFileExtension = "bak";
-    extraSpecialArgs = {
-      inherit
-        inputs
-        self
-        inputs'
-        self'
-        ;
-    };
-
-    users.comfy = ./comfy;
-  };
 }
