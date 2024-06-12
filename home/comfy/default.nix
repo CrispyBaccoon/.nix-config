f3bf8f5b68a7d05d9c2e7295627324aef4e91b1c
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-12 23:44:41 +0200

    fixup! fix!: don't use system to rebuild home

--- home/comfy/default.nix
+++ home/comfy/default.nix
@@ -6,6 +6,7 @@
   # You can import other home-manager modules here
   imports = [
     self.homeManagerModules.default
+    inputs.stylix.homeManagerModules.stylix
     inputs.nix-colors.homeManagerModules.default
 
     ./system
