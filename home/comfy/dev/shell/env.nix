d8a7eed902e00f00ce1c99f635d3ffc58e653b20
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-24 08:11:13 +0200

    fix(dev/shell): source env before `~/.zshrc`

--- home/comfy/dev/shell/env.nix
+++ home/comfy/dev/shell/env.nix
@@ -1,9 +1,12 @@
-{pkgs, ...}: {
-  xdg.configFile."lime/env/init.zsh" = {
+{pkgs, config, ...}: {
+  xdg.configFile."env/init.zsh" = {
     text =
       ''
         export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive"
       ''
       + (builtins.readFile ./env.sh);
   };
+  programs.zsh.envExtra = ''
+    source ${config.xdg.configHome}/env/init.zsh
+    '';
 }
