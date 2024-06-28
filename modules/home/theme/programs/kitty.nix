f3ce11f3a91acbc434fb4148b418bcc4c66fbae6
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-28 08:59:39 +0200

    fix(theme/kitty): simplify kitty theming

--- modules/home/theme/programs/kitty.nix
+++ modules/home/theme/programs/kitty.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   inherit (lib) mkIf;
-  inherit (lib.custom) disabled mkEnableOpt;
+  inherit (lib.custom) disabled mkEnableOpt mapAttrsToCfg mkBase16;
   inherit (config) palette;
   cfg = {
     enable = config.theme.kitty;
@@ -14,28 +14,20 @@ in {
 
   config = mkIf cfg.enable {
     stylix.targets.kitty = disabled;
-    apps.kitty.include = [
+    apps.kitty.include = let
+      base16 =
+        mkBase16 (i: v: {
+          name = "color${i}";
+          value = "#${v}";
+        })
+        palette;
+    in [
       (builtins.toFile "kitty-theme-${config.theme.name}" ''
-        foreground = "#${palette.text}";
-        background = "#${palette.mantle}";
-        cursor = "#${palette.text}";
-        color0 = "#${palette.base}";
-        color1 = "#${palette.red}";
-        color2 = "#${palette.green}";
-        color3 = "#${palette.yellow}";
-        color4 = "#${palette.blue}";
-        color5 = "#${palette.pink}";
-        color6 = "#${palette.aqua}";
-        color7 = "#${palette.text}";
-        color8 = "#${palette.surface0}";
-        color9 = "#${palette.orange}";
-        color10 = "#${palette.surface1}";
-        color11 = "#${palette.surface2}";
-        color12 = "#${palette.overlay1}";
-        color13 = "#${palette.subtext1}";
-        color14 = "#${palette.overlay0}";
-        color15 = "#${palette.overlay2}";
-      '')
+          foreground = "#${palette.text}";
+          background = "#${palette.mantle}";
+          cursor = "#${palette.text}";
+        ''
+        + mapAttrsToCfg (n: v: ''${n} = "${v}";'') base16)
     ];
   };
 }
