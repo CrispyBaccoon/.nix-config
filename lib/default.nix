60fe3538a908dd58f48aa0ebff9bb01a26980956
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-29 10:06:31 +0200

    feat(lib/umport): add umport lib fn

--- lib/default.nix
+++ lib/default.nix
@@ -4,6 +4,7 @@
   flake = import ./flake.nix {inherit lib inputs;};
   builders = import ./builders.nix {inherit lib inputs;};
   services = import ./services.nix {inherit lib;};
+  umport = import ./umport.nix {inherit lib;};
   opt = import ./opt.nix {inherit lib;};
   cfg = import ./cfg.nix {inherit lib;};
   theme = import ./theme.nix {inherit lib;};
@@ -12,6 +13,6 @@ in
   # lib.extend (self: super: super // { custom = super; })
   # lib.extend (_: _: services // opt)
   lib.extend (_: super:
-    {custom = flake // builders // services // opt // cfg // theme;}
+    {custom = flake // builders // services // umport // opt // cfg // theme;}
     // inputs.home-manager.lib
     // super)
