fdd687cffe08334c960c923096604ec07bb1163c
comfysage <67917529+comfysage@users.noreply.github.com>
2024-08-04 19:22:56 +0200

    refactor(lib): use `lib.fixedPoints.composeManyExtensions`

--- lib/default.nix
+++ lib/default.nix
@@ -3,18 +3,23 @@
   inputs,
   ...
 }: let
-  flake = import ./flake.nix {inherit lib;};
-  builders = import ./builders.nix {inherit lib inputs;};
-  services = import ./services.nix {inherit lib;};
-  umport = import ./umport.nix {inherit lib;};
-  opt = import ./opt.nix {inherit lib;};
-  cfg = import ./cfg.nix {inherit lib;};
-  theme = import ./theme.nix {inherit lib;};
-
   custom = lib.makeExtensible (
-    self: {
+    self: let
+      lib = self;
+      flake = import ./flake.nix {inherit lib;};
+      builders = import ./builders.nix {inherit lib inputs;};
+      services = import ./services.nix {inherit lib;};
+      umport = import ./umport.nix {inherit lib;};
+      opt = import ./opt.nix {inherit lib;};
+      cfg = import ./cfg.nix {inherit lib;};
+      theme = import ./theme.nix {inherit lib;};
+    in {
       custom = flake // builders // services // umport // opt // cfg // theme;
     }
   );
+
+  ext = lib.fixedPoints.composeManyExtensions [
+    (_: _: lib)
+  ];
 in
-  custom.extend (_: _: lib // inputs.home-manager.lib)
+  custom.extend ext
