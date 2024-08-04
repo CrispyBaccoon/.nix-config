394c76e39a98b4e2c9b0f5fc7341c010988eb2c8
comfysage <67917529+comfysage@users.noreply.github.com>
2024-08-04 19:58:46 +0200

    feat(hosts): move nix options to nix module

--- modules/system/system/nix/default.nix
+++ modules/system/system/nix/default.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   config,
   pkgs,
   lib,
@@ -41,6 +42,14 @@ in {
       ];
     in {
       inherit (cfg) package;
+
+      # This will add each flake input as a registry
+      # To make nix3 commands consistent with your flake
+      registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+      # This will additionally add your inputs to the system's legacy channels
+      # Making legacy nix commands consistent as well, awesome!
+      nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
       settings = {
         experimental-features = "nix-command flakes";
