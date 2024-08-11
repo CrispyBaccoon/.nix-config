a39a984ae2e373e70a0169b738ad991f757999c2
comfysage <67917529+comfysage@users.noreply.github.com>
2024-08-11 13:48:14 +0200

    fix(home): remove duplicate nix options

--- home/default.nix
+++ home/default.nix
@@ -1,28 +1,9 @@
-{self, inputs, pkgs, ...}: {
+{ self, inputs, ... }: {
   imports = [
     self.homeManagerModules.default
   ];
 
   palette = inputs.evergarden.palette;
-
-  nix = {
-    enable = true;
-    package = pkgs.lix;
-    settings.use-xdg-base-directories = true;
-  };
-
-  nixpkgs = {
-    overlays = [
-      self.outputs.overlays.additions
-      self.outputs.overlays.modifications
-      self.outputs.overlays.unstable-packages
-    ];
-    config = {
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
-  };
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
